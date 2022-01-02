35f3a0b760a15cb63dd36591ce60f37b80b3bdf5
Kevin Aguirre <kevinnaguirre9@gmail.com>
2022-01-02 17:25:04 -0500

    API logout and user info routes created

--- routes/api.php
+++ routes/api.php
@@ -3,18 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/user',  function (Request $request) {
-        return 'Private route';
-    });
+    Route::get('/user',[UserController::class, 'show']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
-
-
-
-/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-}); */
