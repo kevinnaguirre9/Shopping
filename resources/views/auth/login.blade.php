4867371fba5320bccdb298bd7495ce7eb01a1f53
Sergio Díaz <codediaz@outlook.es>
2022-01-03 00:13:02 -0500

    Login Frontend changes - UX

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
+<div class="container ">
+    <div class="row justify-content-center mt-5">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
                 <div class="card-body">
+
+                    <div class="container">
+                        <div class="col-md-8 mb-3" style="margin-left:38%">
+                            <img src=" {{ URL::to('/img/logo-daf.png') }}" alt="Sin Imagen" style="width: 250px">
+
+                        </div>
+                    </div>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-5">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -28,7 +33,7 @@
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-5">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
@@ -52,16 +57,28 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-8 offset-md-5">
+                                <button type="submit" class="btn btn-light" style="width: 200px; background-color: #FF7800 ; border: #FF7800 ; color: white">
                                     {{ __('Login') }}
                                 </button>
 
+                             </div>
+                        </div>
+                        <div class="row mb-0 mt-3">
+                            <div class="col-md-8 offset-md-5">
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <!--a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
-                                    </a>
+                                    </a-->
                                 @endif
+
+                                @if (Route::has('register'))
+                                    Aún no tienes cuenta?
+                                    <a class="btn btn-link" href="{{ route('register') }}">
+                                        {{ __('Registrate') }}
+                                    </a>
+                            
+                            @endif
                             </div>
                         </div>
                     </form>
